655b03c273bedf1b2c5914fe1235e1edc0700862
Shreyansh Singh <91277635+SinghShreyansh@users.noreply.github.com>
2023-06-06 17:53:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import Routes from './routes/route.js';
 const app = express();
 
 
-app.use(cors());
+// app.use(cors());
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }))
